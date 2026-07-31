47a44ec43b554660f36d86f9649d5209b94fbfa2
TJ <tjxjnoobie@gmail.com>
2026-07-31 01:02:28 -0700

    Added: Define PostgreSQL JPA transaction access

---
+++ tavall-database-postgres/src/main/java/org/tavall/database/postgres/jpa/IPostgresJpaHandler.java
@@ -0,0 +1,37 @@
+package org.tavall.database.postgres.jpa;
+
+import jakarta.persistence.EntityManager;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public interface IPostgresJpaHandler extends AutoCloseable {
+
+    boolean isConfigured();
+
+    boolean isOpen();
+
+    <T> T read(Function<EntityManager, T> operation);
+
+    default void read(Consumer<EntityManager> operation) {
+        Objects.requireNonNull(operation, "operation");
+        read(entityManager -> {
+            operation.accept(entityManager);
+            return null;
+        });
+    }
+
+    <T> T write(Function<EntityManager, T> operation);
+
+    default void write(Consumer<EntityManager> operation) {
+        Objects.requireNonNull(operation, "operation");
+        write(entityManager -> {
+            operation.accept(entityManager);
+            return null;
+        });
+    }
+
+    @Override
+    void close();
+}
